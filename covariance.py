3d358da0626bab7f0af7eedf10a9a55b619d5fe1
TY-JI <yenjitang@gmail.com>
2026-09-12 14:55:39 +0100

    test

--- covariance.py
+++ covariance.py
@@ -8,39 +8,44 @@ class Covariance:
         n = T - 1
 
         S = self.calculate_sample_covariance(demeaned_returns, n)
-        F = self.calculate_target_matrix(S, N)
-        pi_hat = self.calculate_pi_hat(demeaned_returns,S, T)
+        F, r_bar = self.calculate_target_matrix(S, N)
+        pi_hat = self.calculate_pi_hat(
+            demeaned_returns,
+            S,
+            N
+        )
 
         print("Pi hat:", pi_hat)
         print(F)
+        print(r_bar)
+        print(self.calculate_rho(demeaned_returns,S,T))
 
-    def calculate_pi_hat(self, demeaned_returns, S, T):
-        covariance_contributions = (self.calculate_covariance_contributions(demeaned_returns))
-        deviation = self.calculate_covariance_deviations(S,covariance_contributions)
+    def calculate_pi_hat_matrix(self, demeaned_returns, S, T):
+        deviation = self.calculate_covariance_deviations(S,demeaned_returns)
 
         squared_deviation = deviation ** 2
         pi_hat_matrix = squared_deviation.sum(axis=0) / T
-        pi_hat = pi_hat_matrix.sum()
+        return pi_hat_matrix
 
+    def calculate_pi_hat(self, demeaned_returns, S, T): 
+        pi_hat = self.calculate_pi_hat_matrix(demeaned_returns, S,T).sum()
         return pi_hat
 
-    def demean(self, returns):
-        return returns - returns.mean()
+    def calculate_rho(self, demeaned_returns, S, T):
+        rho_diag = np.diag(self.calculate_pi_hat_matrix(demeaned_returns, S, T)).sum()
+
+        theta = np.einsum(
+            'ti,tij->ij',
+            (demeaned_returns.to_numpy()**2 - np.diag(S)),
+            (self.calculate_covariance_deviations(S,demeaned_returns))
+        )
+
+        theta = np.fill_diagonal(theta,0)
+
+        return theta
 
     def calculate_sample_covariance(self, demeaned_returns, n):
         return (demeaned_returns.T @ demeaned_returns) / n
-
-    def calculate_covariance_contributions(self, demeaned_returns):
-        covariance_contributions = np.einsum(
-            'ti,tj->tij',
-            demeaned_returns,
-            demeaned_returns
-        )
-        return covariance_contributions
-
-    def calculate_covariance_deviations(self,S,covariance_contributions):
-        deviation = covariance_contributions - S.to_numpy()
-        return deviation
 
     def calculate_target_matrix(self, S, N):
         variances = np.diag(S)
@@ -58,16 +63,28 @@ class Covariance:
         correlation_matrix = S / standard_deviations_outer
         # Recovering correlation matrix
 
-        r_bar = (np.sum(correlation_matrix) - N) / (N*(N - 1))
+        r_bar = (np.sum(correlation_matrix.to_numpy()) - N) / (N*(N - 1))
         # - N eliminates 1s along the diagonal and since there are N - 1 other asses for every asset we have N(N - 1) in the denominator
         
         F = r_bar * standard_deviations_outer
         np.fill_diagonal(F, variances)
 
-        return F
+        return F, r_bar
 
+    def calculate_covariance_contributions(self, demeaned_returns):
+        covariance_contributions = np.einsum(
+            'ti,tj->tij',
+            demeaned_returns,
+            demeaned_returns
+        )
+        return covariance_contributions
 
+    def calculate_covariance_deviations(self,S,demeaned_returns):
+        deviation = self.calculate_covariance_contributions(demeaned_returns) - S.to_numpy()
+        return deviation
 
+    def demean(self, returns):
+        return returns - returns.mean()
 
 
 
