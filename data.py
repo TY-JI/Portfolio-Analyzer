4a7de41823675ae58bbcbca72641e0940d629601
TY-JI <yenjitang@gmail.com>
2026-09-14 22:50:32 +0100

    inclusion of the following new files optimizer.py blackLitterman.py equilibriumReturns.py

--- data.py
+++ data.py
@@ -1,11 +1,15 @@
 import argparse
 import yfinance as yf
+import numpy as np
 
 # TODO
 # add proper error handling for detected correctly formatted date inputs
 # add option to select desired time frame
 
 class MarketDataLoader:
+
+    def __init__(self):
+        self.args = self.parse_args()
 
     def parse_args(self):
         parser = argparse.ArgumentParser()
@@ -55,19 +59,28 @@ class MarketDataLoader:
         return parser.parse_args()
 
     def download(self):
-        args = self.parse_args()
-
         data = yf.download(
-            tickers=args.tickers,
-            start=args.start,
-            end=args.end,
-            period=args.period,
-            interval=args.interval,
+            tickers=self.args.tickers,
+            start=self.args.start,
+            end=self.args.end,
+            period=self.args.period,
+            interval=self.args.interval,
             auto_adjust=True,
             progress=True,
         )
 
         return data
+
+    def market_caps(self):
+        args = self.args
+
+        market_caps = []
+
+        for ticker in self.args.tickers:
+            info = yf.Ticker(ticker).fast_info
+            market_caps.append(info['market_cap'])
+
+        return np.asarray(market_caps, dtype=float)
         
     def get_close_prices(self, data):
         return data['Close']
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from data import MarketDataLoader
 from validation import DataValidator
 from returns import Returns
 from covariance import Covariance
+from blackLitterman import BlackLitterman
+from equilibriumReturns import EquilibriumReturns
+from optimizer import Optimizer
 
 def main():
-
     # Loading market data
     loader = MarketDataLoader()
     data = loader.download()
@@ -25,9 +27,25 @@ def main():
     covariance = Covariance()
     ledoit_wolf = covariance.ledoit_wolf(returns)
     sample_cov_matrix = covariance.sample_covariance(returns,returns.shape[0]-1)
-    print(f'ledoit wolf: {ledoit_wolf}')
-    print(f'sample covariance matrix: {sample_cov_matrix}')
+    #print(f'ledoit wolf: {ledoit_wolf}')
+    #print(f'sample covariance matrix: {sample_cov_matrix}')
 
+    eq = EquilibriumReturns(covariance=ledoit_wolf)
+
+    market_caps = loader.market_caps()
+    market_weights = eq.market_weights(market_caps)
+
+    bl = BlackLitterman(
+        covariance=ledoit_wolf,
+        pi=eq.pi(market_caps)
+    )
+    expected_returns = bl.expected_returns()
+    risk_aversion = eq.risk_aversion(market_weights)
+
+    opt = Optimizer(expected_returns,ledoit_wolf,risk_aversion)
+
+    print(f'Market weights: {market_weights}')
+    print(f'Optimized weights: {opt.optimize()}')
 
 if __name__=='__main__':
     main()
